72c1c5abda3a5e6e5dcdbdbdb2b064e4f48c8b08
kamaevaju <107322383+kamaevaju@users.noreply.github.com>
2022-11-19 06:54:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ function App() {
       const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${MY_ID}&app_key=${MY_KEY}`);
       const data = await response.json();
       setMyRecipies(data.hits); 
-      console.log(data.hits) 
       setMyFood(data.hits); 
     }  
     getFunction();    
